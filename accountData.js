02f6ab71cdbdd5cfca28adb9376636234c478bdb
gttcbendkdj <yfycgu52725@126.com>
2025-10-04 08:14:34 +0800

    Add files via upload

---
+++ accountData.js
@@ -0,0 +1,13 @@
+// 账号数据存储文件，仅存放账号数组，可直接编辑更新
+const accountList = [
+    { account: "user001", password: "Pass@123456" },
+    { account: "user002", password: "Pass@654321" },
+    { account: "user003", password: "Pass@888888" },
+    { account: "user004", password: "Pass@666666" },
+    { account: "user005", password: "Pass@999999" },
+    { account: "user006", password: "Pass@202401" },
+    { account: "user007", password: "Pass@202402" },
+    { account: "user008", password: "Pass@202403" },
+    { account: "user009", password: "Pass@202404" },
+    { account: "user010", password: "Pass@202405" }
+];
